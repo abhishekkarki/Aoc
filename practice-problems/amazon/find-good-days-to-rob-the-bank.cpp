67df7ece1a8f089926af124ca3a89374729abccb
abhishek karki <abhishekkarki202@gmail.com>
2025-01-23 16:54:24 +0100

    correction

--- practice-problems/amazon/find-good-days-to-rob-the-bank.cpp
+++ practice-problems/amazon/find-good-days-to-rob-the-bank.cpp
@@ -1,34 +1,34 @@
-// Find good days to rob the bank
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 vector<int> goodDaysToRobBank(vector<int>& security, int time) {
-    // use two list one keeps the prefix and other will keep the suffix
     int n = security.size();
-    vector<int> prefix(n, 1);
-    vector<int> suffix(n, 1);
+    vector<int> prefix(n, 0);
+    vector<int> suffix(n, 0);
     vector<int> result;
 
+    // take the prefix from left to right
     for (int i = 1; i < n; ++i) {
         if (security[i] <= security[i - 1]) {
-            prefix[i] = security[i - 1] + 1;
-        }
+            prefix[i] = prefix[i - 1] + 1; 
+        } 
     }
 
+    // take the suffix from right to left
     for (int i = n - 2; i >= 0; --i) {
         if (security[i] <= security[i + 1]) {
-            suffix[i] = security[i + 1] + 1;
+            suffix[i] = suffix[i + 1] + 1;
         }
     }
 
+    // if the prefix and suffix are more than the defined time then return them
     for (int i = 0; i < n; ++i) {
-        if (prefix[i] >= time + 1 && suffix[i] >= time + 1) {
+        if (prefix[i] >= time && suffix[i] >= time) {
             result.push_back(i);
         }
     }
-
     return result;
 }
 
@@ -37,7 +37,9 @@ int main() {
     int time = 2;
     vector<int> result = goodDaysToRobBank(days, time);
 
+    cout << "Good days to rob the bank: ";
     for (const int& i : result) {
         cout << i << " ";
-    } cout << endl;
+    }
+    cout << endl;
 }
